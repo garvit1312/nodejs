0fedc8f3e9c1579691e7a6d37e4ae1cd76956567
garvit1312 <guptagarvit131299>
2023-11-28 14:23:50 +0530

    reading project

---
+++ reading.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const fs = require('fs');
+const { buffer } = require('stream/consumers');
+const { clear } = require('console');
+var msg;
+const server  = http.createServer(( req, res )=>{
+
+    const method = req.method;
+    const url = req.url;
+
+    if(url ==='/'){
+        fs.readFile('message.txt',(err, data)=>{
+            if(err){
+                console.log(err);
+                return;
+            }
+            console.log(data);
+            msg = data;
+            res.write('<html>')
+            res.write('<head>Enter Message</head>')
+            res.write(`<body><p>${msg}</p><form action= "/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>`)
+            res.write('</html>')
+            return res.end();
+        });
+
+    }
+
+    if(url === '/message' && method ==='POST'){
+        const body = [];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+
+        });
+        return req.on( 'end', ()=>{
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split('=')[1]
+            fs.writeFile('message.txt', message , (err) =>{
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        })
+    }
+});
+// Listen on port 3000
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
