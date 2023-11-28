8266c47fd9388c8f359e054789704d7e853db572
garvit1312 <guptagarvit131299>
2023-11-28 19:22:49 +0530

    nodemon and package json

--- routes.js
+++ routes.js
@@ -27,6 +27,7 @@ if(url === '/message' && method ==='POST'){
         });
     })
 }
+
 res.setHeader( 'Content-Type','text/html');
 res.write('<html>');
 res.write('<head><title>My First Page</title><head>');
